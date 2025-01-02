acb8d24ba8cb03c129d52af21e39699bd08236fd
vedan102 <vedan102@amd.com>
2025-01-02 09:16:15 +0000

    Snappy: Optimized DecompressBranchless_avx2()     - Added call to optimized `AOCL_DecompressBranchless_avx2()` in decompress function.     - Provided cmake option `SNAPPY_ENABLE_DECOMPRESS_BRANCHLESS` to enable/disable the use of DecompressBranchless in OPT_ON code path.
    AMD-Internal Jira Ticket: [CPUPL-5733]

--- algos/snappy/snappy.cc
+++ algos/snappy/snappy.cc
@@ -4564,6 +4564,177 @@ char* AOCL_CompressFragment_crc32(const char* input, size_t input_size, char* op
 }
 #endif /* AOCL_SNAPPY_AVX_OPT */
 
+#ifdef AOCL_SNAPPY_OPT
+#ifdef AOCL_SNAPPY_ENABLE_DECOMPRESS_BRANCHLESS
+#if defined(__GNUC__) && !defined(__clang__)
+/**
+ * The ternary operators used in the reference code cause slower code execution 
+ * when compiled with GCC as the generated assembly code produces branch instructions 
+ * instead of conditional move instructions. Therefore, to optimize the code,
+ * it is beneficial to replace the ternary operators with inline assembly that makes
+ * use of conditional move instructions.
+ */
+SNAPPY_ATTRIBUTE_ALWAYS_INLINE
+inline size_t AOCL_AdvanceToNextTagX86Optimized_gcc(const uint8_t** ip_p, size_t* tag) {
+  const uint8_t*& ip = *ip_p;
+  size_t tag_type = *tag & 3;
+
+  size_t len;
+  __asm__ __volatile__(
+    "cmp $0, %1\n\t"           // compare tag_type with 0
+    "cmove %2, %0\n\t"         // if tag_type == 0, move lit_len (*tag >> 2)+1 to `len`
+    "cmovne %3, %0\n\t"        // if tag_type != 0, move tag_type to `len`
+    : "=&r"(len)               // output: len
+    : "r"(tag_type), "r"((*tag >> 2) + 1), "r"(tag_type)  // input: tag_type, (*tag >> 2)+1, tag_type
+    : "cc"
+  );
+  ip += (len + 1);
+  *tag = ip[-1];
+  return tag_type;
+}
+#endif
+
+/**
+ * The use of lookup table for extracting the offset results in better
+ * performance than using memcpy.
+ */
+inline uint32_t AOCL_ExtractOffset(uint32_t val, size_t tag_type) {
+#if defined(__aarch64__)
+  constexpr uint64_t kExtractMasksCombined = 0x0000FFFF00FF0000ull;
+  return val & static_cast<uint32_t>(
+      (kExtractMasksCombined >> (tag_type * 16)) & 0xFFFF);
+#else
+  static constexpr uint32_t kExtractMasks[4] = {0, 0xFF, 0xFFFF, 0};
+  return val & kExtractMasks[tag_type];
+#endif
+}
+
+/**
+ * Same as DecompressBranchless_avx2, but calls AOCL_AdvanceToNextTagX86Optimized
+ * (when compiled with GCC) and AOCL_ExtractOffset.
+ */
+template <typename T>
+AOCL_SNAPPY_TARGET_AVX2
+std::pair<const uint8_t*, ptrdiff_t> AOCL_DecompressBranchless_avx2(
+    const uint8_t* ip, const uint8_t* ip_limit, ptrdiff_t op, T op_base,
+    ptrdiff_t op_limit_min_slop) {
+  // If deferred_src is invalid point it here.
+  uint8_t safe_source[64];
+  const void* deferred_src;
+  size_t deferred_length;
+  ClearDeferred(&deferred_src, &deferred_length, safe_source);
+
+  // We unroll the inner loop twice so we need twice the spare room.
+  op_limit_min_slop -= kSlopBytes;
+  if (2 * (kSlopBytes + 1) < ip_limit - ip && op < op_limit_min_slop) {
+    const uint8_t* const ip_limit_min_slop = ip_limit - 2 * kSlopBytes - 1;
+    ip++;
+    // ip points just past the tag and we are touching at maximum kSlopBytes
+    // in an iteration.
+    size_t tag = ip[-1];
+#if defined(__clang__) && defined(__aarch64__)
+    // Workaround for https://bugs.llvm.org/show_bug.cgi?id=51317
+    // when loading 1 byte, clang for aarch64 doesn't realize that it(ldrb)
+    // comes with free zero-extension, so clang generates another
+    // 'and xn, xm, 0xff' before it use that as the offset. This 'and' is
+    // redundant and can be removed by adding this dummy asm, which gives
+    // clang a hint that we're doing the zero-extension at the load.
+    asm("" ::"r"(tag));
+#endif
+    do {
+      // The throughput is limited by instructions, unrolling the inner loop
+      // twice reduces the amount of instructions checking limits and also
+      // leads to reduced mov's.
+
+      SNAPPY_PREFETCH(ip + 128);
+      for (int i = 0; i < 2; i++) {
+        const uint8_t* old_ip = ip;
+        assert(tag == ip[-1]);
+        // For literals tag_type = 0, hence we will always obtain 0 from
+        // ExtractLowBytes. For literals offset will thus be kLiteralOffset.
+        ptrdiff_t len_minus_offset = kLengthMinusOffset[tag];
+        uint32_t next;
+#if defined(__aarch64__)
+        size_t tag_type = AdvanceToNextTagARMOptimized(&ip, &tag);
+        // We never need more than 16 bits. Doing a Load16 allows the compiler
+        // to elide the masking operation in ExtractOffset.
+        next = LittleEndian::Load16(old_ip);
+#else
+#if defined(__GNUC__) && !defined(__clang__)
+        size_t tag_type = AOCL_AdvanceToNextTagX86Optimized_gcc(&ip, &tag);
+#else
+        size_t tag_type = AdvanceToNextTagX86Optimized(&ip, &tag);
+#endif
+        next = LittleEndian::Load32(old_ip);
+#endif
+        size_t len = len_minus_offset & 0xFF;
+        ptrdiff_t extracted = AOCL_ExtractOffset(next, tag_type);
+        ptrdiff_t len_min_offset = len_minus_offset - extracted;
+        if (SNAPPY_PREDICT_FALSE(len_minus_offset > extracted)) {
+          if (SNAPPY_PREDICT_FALSE(len & 0x80)) {
+            // Exceptional case (long literal or copy 4).
+            // Actually doing the copy here is negatively impacting the main
+            // loop due to compiler incorrectly allocating a register for
+            // this fallback. Hence we just break.
+          break_loop:
+            ip = old_ip;
+            goto exit;
+          }
+          // Only copy-1 or copy-2 tags can get here.
+          assert(tag_type == 1 || tag_type == 2);
+          std::ptrdiff_t delta = (op + deferred_length) + len_min_offset - len;
+          // Guard against copies before the buffer start.
+          // Execute any deferred MemCopy since we write to dst here.
+          MemCopy64_avx2(op_base + op, deferred_src, deferred_length);
+          op += deferred_length;
+          ClearDeferred(&deferred_src, &deferred_length, safe_source);
+          if (SNAPPY_PREDICT_FALSE(delta < 0 ||
+                                  !Copy64BytesWithPatternExtension(
+                                      op_base + op, len - len_min_offset))) {
+            goto break_loop;
+          }
+          // We aren't deferring this copy so add length right away.
+          op += len;
+          continue;
+        }
+        std::ptrdiff_t delta = (op + deferred_length) + len_min_offset - len;
+        if (SNAPPY_PREDICT_FALSE(delta < 0)) {
+          // Due to the spurious offset in literals have this will trigger
+          // at the start of a block when op is still smaller than 256.
+          if (tag_type != 0) goto break_loop;
+          MemCopy64_avx2(op_base + op, deferred_src, deferred_length);
+          op += deferred_length;
+          DeferMemCopy(&deferred_src, &deferred_length, old_ip, len);
+          continue;
+        }
+
+        // For copies we need to copy from op_base + delta, for literals
+        // we need to copy from ip instead of from the stream.
+        const void* from =
+            tag_type ? reinterpret_cast<void*>(op_base + delta) : old_ip;
+        MemCopy64_avx2(op_base + op, deferred_src, deferred_length);
+        op += deferred_length;
+        DeferMemCopy(&deferred_src, &deferred_length, from, len);
+      }
+    } while (ip < ip_limit_min_slop &&
+             static_cast<ptrdiff_t>(op + deferred_length) < op_limit_min_slop);
+  exit:
+    ip--;
+    assert(ip <= ip_limit);
+  }
+  // If we deferred a copy then we can perform.  If we are up to date then we
+  // might not have enough slop bytes and could run past the end.
+  if (deferred_length) {
+      MemCopy64_avx2(op_base + op, deferred_src, deferred_length);
+    op += deferred_length;
+    ClearDeferred(&deferred_src, &deferred_length, safe_source);
+  }
+  return {ip, op};
+}
+
+#endif /* AOCL_SNAPPY_ENABLE_DECOMPRESS_BRANCHLESS */
+#endif /* AOCL_SNAPPY_OPT */
+
 #ifdef AOCL_SNAPPY_AVX2_OPT
 template<>
 template<>
@@ -4604,6 +4775,21 @@ void SnappyDecompressor<with_bmi_avx>::DecompressAllTags_bmi<AOCL_SnappyArrayWri
     __asm__(".p2align 3");
 #endif
     for ( ;; ) {
+#ifdef AOCL_SNAPPY_ENABLE_DECOMPRESS_BRANCHLESS
+      {
+        ptrdiff_t op_limit_min_slop;
+        auto op_base = writer->GetBase(&op_limit_min_slop);
+        if (op_base) {
+          auto res =
+              AOCL_DecompressBranchless_avx2<decltype(op_base)>(reinterpret_cast<const uint8_t*>(ip),
+                                   reinterpret_cast<const uint8_t*>(ip_limit_),
+                                   op - op_base, op_base, op_limit_min_slop);
+          ip = reinterpret_cast<const char*>(res.first);
+          op = op_base + res.second;
+          MAYBE_REFILL();
+        }
+      }
+#endif /* AOCL_SNAPPY_ENABLE_DECOMPRESS_BRANCHLESS */
       const uint8_t c = static_cast<uint8_t>(preload);
       ip++;
 
