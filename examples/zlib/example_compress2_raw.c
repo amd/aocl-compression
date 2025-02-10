67728172fb0224eed65c699b9326346ace632468
Sriram <assriram@amd.com>
2025-02-10 18:47:20 +0530

    Examples are added for new GZIP and deflate apis for multithreaded compression/decompression     Two new examples files are added:         - example_compress2_gzip which uses: compress2_gzip, compressBound_gzip, uncompress2_gzip.         - example_compress2_raw which uses: compress2_raw, compressBound, uncompress2_raw.

--- examples/zlib/example_compress2_raw.c
+++ examples/zlib/example_compress2_raw.c
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2024-2025, Advanced Micro Devices. All rights reserved.
+ * Copyright (C) 2025, Advanced Micro Devices. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -26,33 +26,33 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
- /** @file example_snappy_compress.c
+ /** @file example_compress2_raw.c
  *
- *  @brief Example for AOCL-Compression snappy native API.
+ *  @brief Example for AOCL-Compression zlib native API.
  *
- *  The following test program shows the sample usage and calling sequence of snappy
+ *  The following test program shows the sample usage and calling sequence of zlib
  *  native API within AOCL - compression to compress and decompress a test input
  *
  *  @note To run the program:
- *  example_snappy_compress <input_file_to_be_compressed>
+ *  example_compress2_raw <input_file_to_be_compressed>
  * 
- *  @author Ashish Sriram
+ *  @author Niranjan Reddy
  */
-
-#include <stdio.h> 
+#include <stdio.h>
 #include <stdlib.h>
-#include "snappy-c.h"
+#include "zlib.h"
 
 int main(int argc, char** argv)
 {
     FILE* inFp = NULL;
-    size_t inpSize = 0;
-    char* inPtr = NULL, * compPtr = NULL, * decompPtr = NULL;
-    size_t outSize = 0;
-    snappy_status resultComp, resultDecomp;
+    uLong inpSize = 0;
+    Bytef* inPtr = NULL, * compPtr = NULL, * decompPtr = NULL;
+    uLong outSize = 0;
+    int resultComp, resultDecomp;
+    int level = 6;
 
-    printf("Running example_snappy_compress\n");
-    printf("Demonstrates using native APIs for Snappy compression and decompression\n");
+    printf("Running example_compress2_raw\n");
+    printf("Demonstrates using native APIs for raw compression and decompression\n");
     if (argc < 2)
     {
         printf("Provide input test file path\n");
@@ -64,20 +64,20 @@ int main(int argc, char** argv)
     rewind(inFp);
 
     // 1. allocate buffers
-    outSize = snappy_max_compressed_length(inpSize);
+    outSize = compressBound(inpSize);
     if (outSize == 0)
     {
-        printf("CompressBound: failed\n");
+        printf("compressBound: failed\n");
         goto error_exit;
     }
-    inPtr = (char*)calloc(1, inpSize);
-    compPtr = (char*)calloc(1, outSize);
-    decompPtr = (char*)calloc(1, inpSize);
+    inPtr     = (Bytef*)calloc(1, inpSize);
+    compPtr   = (Bytef*)calloc(1, outSize);
+    decompPtr = (Bytef*)calloc(1, inpSize);
     inpSize = fread(inPtr, 1, inpSize, inFp);
 
     // 2. compress
-    resultComp = snappy_compress(inPtr, inpSize, compPtr, &outSize);
-    if (resultComp != SNAPPY_OK)
+    resultComp = compress2_raw(compPtr, &outSize, inPtr, inpSize, level);
+    if (resultComp != Z_OK)
     {
         printf("Compression: failed\n");
         goto error_exit;
@@ -85,8 +85,8 @@ int main(int argc, char** argv)
     printf("Compression: done\n");
 
     // 3. decompress
-    resultDecomp = snappy_uncompress(compPtr, outSize, decompPtr, &inpSize);
-    if (resultDecomp != SNAPPY_OK)
+    resultDecomp = uncompress2_raw(decompPtr, &inpSize, compPtr, &outSize);
+    if (resultDecomp != Z_OK)
     {
         printf("Decompression Failure\n");
         goto error_exit;
